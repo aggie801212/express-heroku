3fb9e400ad0380321b20ba13e6e6c22ce9a2016f
aggie801212 <aggie1236@gmail.com>
2017-09-11 22:00:37 +0800

    add dynamic menu

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const getAddress = require('./get-address');
 const app = express()
 const port = process.env.PORT || 3000
 
+//定義使用ejs
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/home', function (req, res) {
+  res.render('home', {
+    title: 'hello world',
+    menu: ['abc','bcd','efg'],
+  }); 
+//           home 路徑預設在views(需要自己新增資料夾)裡面
+// 傳值必須用一個object傳入
+// ejs會對應傳入的變數名稱對應替換內容
+})
 
 app.get('/query-address', function (req, res) {
   let address = req.query.address
@@ -26,3 +38,17 @@ app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
 })
 
+//ejs note
+//.ejs 內容等於 .html 的格式 只是增加挖空塞變數的地方
+//ejs 使用 <% var %> 來使用塞變數或是做判斷式也可以
+
+//ex:
+
+// <% if (user) { %>
+//   <h2><%= user.name %></h2>
+// <% } %>
+// <ul>
+//   <% users.forEach(function(user){ %>
+//     <%- include('user/show', {user: user}); %>
+//   <% }); %>
+// </ul>
