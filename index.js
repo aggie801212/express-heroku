b7a320103815c1d3d875f0f7fa39860a456ad332
aggie801212 <aggie1236@gmail.com>
2017-09-11 20:43:06 +0800

    add a parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const request = require('request');
+const getAddress = require('./get-address');
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -8,15 +10,15 @@ app.get('/', function (req, res) {
 })
 
 
-app.get('/hi', function (req, res) {
-  let address = 'National Taiwan University'
+app.get('/query-address', function (req, res) {
+  let address = req.query.address
   let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
   request(url, 
       function (error, response, body) {
     console.log('error:', error);
     console.log('statusCode:', response.statusCode);
     console.log('body:', body);
-    res.send(body)
+    res.send(getAddress(JSON.parse(body)));//字串轉成object
 });
 })
 
